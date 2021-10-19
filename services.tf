253ff7b9653e66487da40cffd3684f2ba178d0be
MattSnyk <74914681+MattSnyk@users.noreply.github.com>
2021-10-19 09:21:39 +0100

    Add files via upload

---
+++ services.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+ root_block_device {
+#    volume_size           = "20"
+    volume_type           = "gp2"
+    encrypted             = false
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket" "pleasebedamnuniquealready" {
+  bucket = "pleasebedamnuniquealready"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+
+}
