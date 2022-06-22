5ea754e53e497f0423b7d57ad25dbcf82e521397
Troy Havelock <troy.havelock@snyk.io>
2022-06-22 12:23:30 -0400

    Adding new entity

---
+++ entity/Test.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "Test",
+    columns: {
+      id: {
+        primary: true,
+        type: "int",
+        generated: true
+      },
+      name: {
+        type: "varchar"
+      }
+    }
+  };
