926908588f7b62c1c8c2471c87b1f0ca53ed6c24
bochi <boschluckas@gmail.com>
2023-02-24 18:23:32 +0100

    cwe-89 fix

--- routes/index.js
+++ routes/index.js
@@ -231,7 +231,14 @@ exports.edit = function (req, res, next) {
 };
 
 exports.update = function (req, res, next) {
-  Todo.findById(req.params.id, function (err, todo) {
+  const validId = req.params.id.match(/^[0-9a-fA-F]{24}$/) ? req.params.id : null;
+
+  if (!validId) {
+    return res.status(400).send('Invalid ID');
+  }
+
+  Todo.findById(validId, function (err, todo) {
+    if (err) return next(err);
 
     todo.content = req.body.content;
     todo.updated_at = Date.now();
@@ -242,6 +249,7 @@ exports.update = function (req, res, next) {
     });
   });
 };
+
 
 // ** express turns the cookie key to lowercase **
 exports.current_user = function (req, res, next) {
