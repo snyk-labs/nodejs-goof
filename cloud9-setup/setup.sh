2fb89b48ab71df33ea2e5f0acdc8abc06f9122be
Eric <eric.smalling@snyk.io>
2023-06-01 13:28:44 -0500

    The purge

---
+++ cloud9-setup/setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# if kubectl is not installed, install it
+if ! [ -x "$(command -v kubectl)" ]; then
+    echo "kubectl not found, installing..."
+    curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+    chmod +x ./kubectl
+    sudo mv ./kubectl /usr/local/bin/kubectl
+fi
+
+echo "Inatlling/Upgrading awscli..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+echo "Installing jq, gettext, bash-completion, moreutils..."
+sudo apt-get -y install jq gettext bash-completion moreutils
+
+echo "Installing yq for yaml processing..."
+echo 'yq() {
+  docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+}' | tee -a ~/.bashrc && source ~/.bashrc
+
+echo "Enabling kubectl bash_completion..."
+kubectl completion bash >>  ~/.bash_completion
+echo "source <(kubectl completion bash)" >> ~/.bashrc
+
+echo "Aliasing 'k' to 'kubectl'..."
+echo "alias k=kubectl" >> ~/.bashrc
+echo "complete -F __start_kubectl k" >> ~/.bashrc
+
+echo "Done. Please source ~/.bashrc to complete the installation."
