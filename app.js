45ac2f91373ccf0d7181baec92a15a30dbb3daad
Andrew Mathes <100437964+AMathesCheckDRV@users.noreply.github.com>
2024-02-01 21:14:02 -0700

    set recommended SNYK security in app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require('./typeorm-db')
 var st = require('st');
 var crypto = require('crypto');
 var express = require('express');
-var http = require('http');
+var https = require('https');
 var path = require('path');
 var ejsEngine = require('ejs-locals');
 var bodyParser = require('body-parser');
@@ -80,9 +80,9 @@ if (app.get('env') == 'development') {
   app.use(errorHandler());
 }
 
-var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
-console.log('token: ' + token);
+const sessionSecret = process.env.SESSION_SECRET;
+console.log('token: ' + sessionSecret);
 
-http.createServer(app).listen(app.get('port'), function () {
+https.createServer(app).listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
