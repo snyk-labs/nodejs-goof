661e5684c802f99e1f4496476486254e5e1b2888
Troy Havelock <troy.havelock@snyk.io>
2024-03-11 07:52:14 -0400

    Some really important features to add

--- app.js
+++ app.js
@@ -84,6 +84,8 @@ if (app.get('env') == 'development') {
   app.use(errorHandler());
 }
 
+db_password = 'secret_1234567890'
+
 var token = 'SECRET_TOKEN_f8ed84e8f41e4146403dd4a6bbcea5e418d23a9';
 console.log('token: ' + token);
 
