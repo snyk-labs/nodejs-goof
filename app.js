48293a390809ea4dd2da545f1ac684cb9cce621c
mnathsnyk <162179737+mnathsnyk@users.noreply.github.com>
2024-03-14 21:20:55 -0400

    Update app.js
    deleted app.use(session({
      secret: 'keyboard cat',
      name: 'connect.sid',
      cookie: { path: '/' }
    }))

--- app.js
+++ app.js
@@ -39,11 +39,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(methodOverride());
-app.use(session({
-  secret: 'keyboard cat',
-  name: 'connect.sid',
-  cookie: { path: '/' }
-}))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(fileUpload());
