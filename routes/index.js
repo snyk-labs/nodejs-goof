8a035f25951ecd1c3825c8dffab39aba50f41d02
jamieallensnyk <83961092+jamieallensnyk@users.noreply.github.com>
2026-07-15 17:33:13 +0300

    Remove get_account_details function
    Removed placeholder function for getting account details.

--- routes/index.js
+++ routes/index.js
@@ -79,12 +79,7 @@ exports.admin = function (req, res, next) {
   });
 };
 
-exports.get_account_details = function(req, res, next) {
-  // @TODO need to add a database call to get the profile from the database
-  // and provide it to the view to display
-  const profile = {}
- 	return res.render('account.hbs', profile)
-}
+
 
 exports.save_account_details = function(req, res, next) {
   // get the profile details from the JSON
